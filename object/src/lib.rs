c201369776e4263ff208a90c4126a9d23787ed5d
Michael du Plessis <mdp0101@gmail.com>
2025-06-27 16:22:00 +0200

    Implemented method to create objects for the Registry

--- object/src/lib.rs
+++ object/src/lib.rs
@@ -40,6 +40,14 @@ where
     }
 }
 
+/// The kinds of errors that can occur with the registry
+pub enum RegistryError {
+    /// The factory for the TypeId specified does not exist
+    NoFactory,
+    /// The TypeId being registered has already been registered
+    AlreadyRegistered,
+}
+
 /// The type of the factory used in the Registry
 type RegistryObjectFactory = ObjectFactory<Box<dyn Fn(Vec<u8>) -> Box<dyn Object>>>;
 
@@ -51,7 +59,29 @@ struct Registry {
 
 impl Registry {
     /// Adds an ObjectFactory to the Registry
-    pub fn register_factory(&mut self, type_id: TypeId, factory: RegistryObjectFactory) {
-        self.factories.insert(type_id, factory);
+    pub fn register_factory(
+        &mut self,
+        type_id: TypeId,
+        factory: RegistryObjectFactory,
+    ) -> Result<(), RegistryError> {
+        if self.factories.contains_key(&type_id) {
+            Err(RegistryError::AlreadyRegistered)
+        } else {
+            self.factories.insert(type_id, factory);
+            Ok(())
+        }
+    }
+
+    /// Creates an object using the Registry and the associated ObjectFactory if one exists
+    pub fn create_object(
+        &self,
+        type_id: TypeId,
+        bytes: Vec<u8>,
+    ) -> Result<Box<dyn Object>, RegistryError> {
+        if let Some(factory) = self.factories.get(&type_id) {
+            Ok(factory.create_object(bytes))
+        } else {
+            Err(RegistryError::NoFactory)
+        }
     }
 }

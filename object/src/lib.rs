ba46707d6b443b5bccae71896dfaabc216b9b849
Michael du Plessis <mdp0101@gmail.com>
2025-06-30 10:28:17 +0200

    further implemented the Registry and added method to create objects and add objects to the registry

--- object/src/lib.rs
+++ object/src/lib.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{LazyLock, RwLock},
+};
 
 pub mod int;
 pub mod null;
@@ -11,7 +14,7 @@ pub struct Key(String);
 pub type TypeId = u8;
 
 /// Defines and object as well as what methods can be performed on it
-pub trait Object: Send + Sync + std::fmt::Debug {
+pub trait Object: std::fmt::Debug {
     /// Returns the TypeId of the object
     fn type_id(&self) -> TypeId;
 
@@ -19,18 +22,21 @@ pub trait Object: Send + Sync + std::fmt::Debug {
     fn serialize(self) -> Vec<u8>;
 }
 
+/// The type of the object used in the database
+pub type DbObject = Box<dyn Object + Send + Sync>;
+
 /// Used to create Box<dyn Objects>
 #[derive(Debug)]
 pub struct ObjectFactory<F>
 where
-    F: Fn(Vec<u8>) -> Box<dyn Object>,
+    F: Fn(Vec<u8>) -> DbObject,
 {
     factory_method: F,
 }
 
 impl<F> ObjectFactory<F>
 where
-    F: Fn(Vec<u8>) -> Box<dyn Object>,
+    F: Fn(Vec<u8>) -> DbObject,
 {
     /// Creates a new ObjectFactory
     pub fn new(factory_method: F) -> Self {
@@ -38,7 +44,7 @@ where
     }
 
     /// Creates a Box<dyn Object> from some bytes
-    pub fn create_object(&self, bytes: Vec<u8>) -> Box<dyn Object> {
+    pub fn create_object(&self, bytes: Vec<u8>) -> DbObject {
         (self.factory_method)(bytes)
     }
 }
@@ -52,7 +58,7 @@ pub enum RegistryError {
 }
 
 /// The type of the factory used in the Registry
-type RegistryObjectFactory = ObjectFactory<Box<dyn Fn(Vec<u8>) -> Box<dyn Object>>>;
+type RegistryObjectFactory = ObjectFactory<Box<dyn Fn(Vec<u8>) -> DbObject + Send + Sync>>;
 
 /// Contains a mapping of TypeId's to ObjectFactories and is used to ceate Box<dyn Object>'s
 #[derive(Default)]
@@ -80,11 +86,35 @@ impl Registry {
         &self,
         type_id: TypeId,
         bytes: Vec<u8>,
-    ) -> Result<Box<dyn Object>, RegistryError> {
+    ) -> Result<DbObject, RegistryError> {
         if let Some(factory) = self.factories.get(&type_id) {
             Ok(factory.create_object(bytes))
         } else {
             Err(RegistryError::NoFactory)
         }
+    }
+}
+
+/// There should only ever be one registry struct
+static REGISTRY: LazyLock<RwLock<Registry>> = LazyLock::new(Default::default);
+
+/// All methods for a registry
+pub mod type_registry {
+    use crate::{DbObject, REGISTRY, RegistryError, RegistryObjectFactory, TypeId};
+
+    /// Register a new type
+    pub fn register_type(
+        type_id: TypeId,
+        factory: RegistryObjectFactory,
+    ) -> Result<(), RegistryError> {
+        // getting the registry
+        let mut registry = REGISTRY.write().unwrap();
+        registry.register_factory(type_id, factory)
+    }
+
+    /// Create a new object from a TypeId and raw bytes
+    pub fn create_object(type_id: TypeId, bytes: Vec<u8>) -> Result<DbObject, RegistryError> {
+        let registry = REGISTRY.read().unwrap();
+        registry.create_object(type_id, bytes)
     }
 }

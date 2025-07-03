e02ff3250c15d8cb4ba8fac7edeb4391b54f2346
Michael du Plessis <mdp0101@gmail.com>
2025-07-03 22:32:14 +0200

    started implementing main loop

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,25 @@
+use logging::{error, info};
+use server::Server;
+
+/// The port to listen on
+const PORT: u16 = 4871;
+
 fn main() {
-    println!("Hello, world!");
+    info!("Starting server");
+
+    // The server to listen on
+    let mut server = Server::new(PORT);
+
+    info!("Listening on port: {PORT}");
+    loop {
+        let Ok(mut connection) = server.listen() else {
+            error!("Something bad happend");
+            break;
+        };
+
+        loop {
+            let command = connection.recieve();
+        }
+    }
+    
 }
